7fcff16949bf970cececac32050450c8e60e8184
Jacob Vanderlaan <jvander@utah.gov>
2021-06-24 11:01:54 -0600

    Update updatePermits.R

--- R/updatePermits.R
+++ R/updatePermits.R
@@ -10,7 +10,7 @@ updatePermits=function(){
 	library(wasteloadR)
 	permits=wqTools::readECHO_fac(p_st="ut", p_act="y")	
 	permits$properties$prefix=substr(permits$properties$SourceID, 1, 3)
-	permits=subset(permits, properties$prefix %in% c('UT0','UTL'))
+	#permits=subset(permits, properties$prefix %in% c('UT0'))
 	permits_coords=do.call(rbind.data.frame,permits$geometry$coordinates)
 	names(permits_coords)=c("dec_long","dec_lat")
 	permits_coords=data.frame(permits$properties[,c("SourceID","CWPName","CWPFacilityTypeIndicator")], (permits_coords))
@@ -19,6 +19,7 @@ updatePermits=function(){
 	names(permits_coords)[names(permits_coords)=="CWPFacilityTypeIndicator"]="permit_type"
 	names(permits_coords)[names(permits_coords)=="dec_long"]="LongitudeMeasure"
 	names(permits_coords)[names(permits_coords)=="dec_lat"]="LatitudeMeasure"
+	permits_coords$prefix=substr(permits_coords$permit_id, 1, 3)
 	path=paste0(path.package('wasteloadR'),'/extdata')
 	#save(permits_coords, file=paste0(path,"/permits.Rdata"))
 	save(permits_coords, file="C:\\Users\\jvander\\Documents\\R\\wasteloadR\\data\\permits_coords.Rdata")
