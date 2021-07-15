eb0496ddd049e0be52957fd37a65f7e14aa85c37
Jacob Vanderlaan <jvander@utah.gov>
2021-07-15 16:00:49 -0600

    Update mixZone.R

--- R/mixZone.R
+++ R/mixZone.R
@@ -3,26 +3,32 @@
 #' Function returns table with distance from discharge, travel time and plume width
 #' Function returns plume width at 2500 feet distance from discharge and 15 minutes of travel time, expressed as % of stream width
  
-#' @param streamQcfs Upstream discharge in CFS
-#' @param effluentQcfs Effluent discharge in CFS
-#' @param dist_ft Discharge distance from shore in feet.
+#' @param streamQcfs Upstream seasonal 7Q10 critical flow in CFS
+#' @param effluentQcfs Seasonal average effluent discharge in CFS
+#' @param shore_dist_ft Discharge distance from shore in feet.
 #' @param width_ft Stream channel width in feet
 #' @param depth_ft Stream channel depth in feet
-#' @param slope Stream channel slope, unitless
-#' @param mixing_coef Mixing coefficient estimate. Default is 0.6.
-#' @param max_dist_ft Maximum distance to calculate plume width in feet. Default 2500.
-#' @param dist_int_ft Interval distance for plume width calculations in feet. Deafult 500. Plume widths are calculated from dist_int_ft to max_dist_ft with an interval of dist_int_ft.
+#' @param slope Stream channel slope, unitless (typically ft/ft)
+#' @param mix_coeff Mixing coefficient estimate. Default is 0.6.
+#' @param Q_location Discharge channel location. One of "side" (default) or "center".
+
 #
 
 #' @return TBD, table, list, etc
 
-#' @import dplyr
-#' @importFrom lubridate year
-
 #' @examples
 
 #' @export
-mixZone = function(streamQcfs, effluentQcfs, dist_ft, width_ft, depth_ft, slope, mixing_coef=0.6, max_dist_ft=2500, dist_int_ft=500){
+mixZone = function(streamQcfs, effluentQcfs, shore_dist_ft, width_ft, depth_ft, slope, mix_coeff=0.6, Q_location="side"){
+		#streamQcfs=250
+		#effluentQcfs=75
+		#shore_dist_ft=5
+		#width_ft=20
+		#depth_ft=3
+		#slope=0.2
+		#mix_coeff=0.6
+		#Q_location="side"
+		
 		# streamQcfs: upstream seasonal 7Q10 critical Q (with wasteloadR tool pick USGS, DWQ, other sites 20-50 mi upstream using NHD+ reaches)
 			# daily USGS for 7Q10, need sites with n>=32, closest to furthest
 			# if not use (ie: DWQ) with n>=32
@@ -30,7 +36,7 @@ mixZone = function(streamQcfs, effluentQcfs, dist_ft, width_ft, depth_ft, slope,
 		# effluentQcfs: seasonal average effluent discharge or annual (with note) if not seasonal
 			# from facility DMR or ICIS like in wasteloadR tool
 			# maybe a user picker for time range of data (ie: 5 yr, 10 yr, 20 yr) but typically 10 yr
-		# dist_ft: the xlsx code used an iterative 100ft distance to 5000ft and then a vlookup to pull data. We can just code the distance required as below.
+		# shore_dist_ft: Discharge distance from shore in feet.
 		# width_ft: the stream channel width at effluent discharge point
 			# can be user supplied, from NHD+ reach info, USGS stream characteriz, or other
 		# depth_ft: the average stream channel depth at effluent discharge point
@@ -41,6 +47,8 @@ mixZone = function(streamQcfs, effluentQcfs, dist_ft, width_ft, depth_ft, slope,
 			# then calc ave river elev drop (ft/mi) across the distances
 			# if elev drop < 6.2 ft/mi then natural, otherwise channelized
 			# calculate stream sinuosity from (reach distance : linear distance)
+	
+	if(!Q_location%in%c("side","center")){stop("Q_location must be one of c('side','center')")}
 	
 	# Combined Q (upstream & discharge)
 	comb_q=streamQcfs+effluentQcfs
@@ -54,13 +62,13 @@ mixZone = function(streamQcfs, effluentQcfs, dist_ft, width_ft, depth_ft, slope,
 		# Q=(1.49/n)*A*R^(2/3)*S^(1/2) # rearrange to calculate n [mannings_n=(1.49/Q)*A*R^(2/3)*S^(1/2)]
 	area_ft2 = width_ft*depth_ft
 	hyd_rad_ft = 2*depth_ft+width_ft
-	mannings_n=(1.486/comb_q)*area_ft2*hyd_rad_ft^(2/3)*slope^(1/2)]
+	mannings_n=(1.486/comb_q)*area_ft2*hyd_rad_ft^(2/3)*0.01^(1/2)
 	
 	# Check reasonable-ness of mannings_n (expected range = 0.018-0.060)
 		# maybe reference table of mannings n values for user (http://www.fsl.orst.edu/geowater/FX3/help/8_Hydraulic_Reference/Mannings_n_Tables.htm or
 		# https://pubs.usgs.gov/wsp/2339/report.pdf)
-	if(mannings_n<0.018){warning("Manning's n coefficient <0.018")}
-	if(mannings_n>0.060){warning("Manning's n coefficient >0.060")}
+	if(mannings_n<0.018){warning("Manning's n coefficient <0.018. See http://www.fsl.orst.edu/geowater/FX3/help/8_Hydraulic_Reference/Mannings_n_Tables.htm and https://pubs.usgs.gov/wsp/2339/report.pdf for more information.")}
+	if(mannings_n>0.060){warning("Manning's n coefficient >0.060. See http://www.fsl.orst.edu/geowater/FX3/help/8_Hydraulic_Reference/Mannings_n_Tables.htm and https://pubs.usgs.gov/wsp/2339/report.pdf for more information.")}
 	
 	
 	# velocity (ft/sec)
@@ -73,7 +81,8 @@ mixZone = function(streamQcfs, effluentQcfs, dist_ft, width_ft, depth_ft, slope,
 	
 	# longitudinal and lateral dispersion coefficients (Chapra. 1997. Surface Water Quality Modeling. Chap 14.4)
 	## defs: G=, D=, S=, D=, C1=
-		# G: the gravitational acceleration (9.81 m/s2 or 32.174 ft.s2)
+		# G: the gravitational acceleration (32.174 ft.s2)
+		G=32.174 #JV note - set to fixed ft.s2 number.
 		# D: the average stream channel depth; depth_ft
 		# S: the average stream channel slope around the effluent discharge point; S or slope
 		# C1: the mixing coefficient (default value= 0.6 (0.3 - 0.9). Varies with channel irregularity; straight channel uniform flow=0.3, 
@@ -87,67 +96,84 @@ mixZone = function(streamQcfs, effluentQcfs, dist_ft, width_ft, depth_ft, slope,
 	froudeNum = velocity_ftsec / Sqrt(G*depth_ft)
 	
 	## lateral dispersion coefficient (ft2/s)
-	mix_coeff=0.6
+	#mix_coeff=0.6 - default value in function currently. Do we want to user modified? Or permanant?
 	latDispersionCoeff=shearVel*depth_ft*mix_coeff
 	
-	if Q_location = 1
+	if(Q_location == "side"){
 		mixLength=0.4*velocity_ftsec*(width_ft^2 / latDispersionCoeff)
-	if Q_location = 2
+	}
+	if(Q_location == "center"){
 		mixLength=0.1*velocity_ftsec*(width_ft^2 / latDispersionCoeff)
+	}
 	
 	## longitudinal dispersion coefficient (ft2/s)
-	if froudeNum <0.5, then
+	if (froudeNum <0.5){
 		longDispersionCoeff=0.058*comb_q / (slope * width_ft) # (McQuivey and Keefer, 1974)
-	else
+	}
+	else{
 		longDispersionCoeff=0.011*((velocity_ftsec^2 * width_ft^2) / (depth_ft * shearVel) # (Fisher et al., 1979)
+	}
 	
 	# plume width (CHRONIC at 2500 ft & ACUTE at 15 min)
-	## return plume width at multiple distances - user specifies maximum distance and distance interval
-		## Jake, we only need the 2500 ft distance and 15 min period. No need to do multiple distances
 	## defs: x=, W=, D1, X1=, t1, D
-		# x: the distance of effluent discharge from shore in ft (user provided); xShore_ft
+		# x: the distance of effluent discharge from shore in ft (user provided); shore_dist_ft
 		# W: the average stream channel width at effluent discharge point; width_ft
 		# D1: the lateral dispersion coefficient inf ft2/s; latDispersionCoeff
 		# X1: the downstream plume distance, chronic calculated at 2500 ft; xDownChronic_ft
 		# t1: the downstream plume travel time, acute calculated at 15 min; tDownAcute_s
 		# U: the average downstream velocity (ft/s); velocity_ftsec
-	target_distances=seq(dist_int_ft, max_dist_ft, by=dist_int_ft) # if want sequence then the following. Maybe start at -3 intervals
-		# dist_int_ft: the downstream distance interval for the sequence in ft; 5
-		# max_dist_ft: the maximum downstream distance for the sequence in ft; 5280
 	xDownChronic_ft=2500
 	tDownAcute_s=15*60 # in seconds
-	plume_widthChronic_ft=(((2*xShore_ft/width_ft+1)^2)*PI()*latDispersionCoeff*(xDownChronic_ft/velocity_ftsec))^0.5 # can apply equation across all target distances.
-	plume_widthAcute_ft=(((2*xShore_ft/width_ft+1)^2)*PI()*latDispersionCoeff*((/velocity_ftsec*tDownAcute_s))^0.5 # can apply equation across all target distances.
+	plume_widthChronic_ft=(((2*shore_dist_ft/width_ft+1)^2)*PI()*latDispersionCoeff*(xDownChronic_ft/velocity_ftsec))^0.5
+	plume_widthAcute_ft=(((2*shore_dist_ft/width_ft+1)^2)*PI()*latDispersionCoeff*((/velocity_ftsec*tDownAcute_s))^0.5
 					   
 	# Chronic theta or plume width as percent of river at 2500 ft and 15 min
-	## defs: x=, W=, D1, X2=, D
 		# X1: the downstream plume distance, chronic calculated at 2500 ft; xDownChronic_ft
 		# plume width: the plume width across the stream at chronic distance of effluent discharge (2500 ft) in ft; plume_width_ft
 		# W: the average stream channel width at effluent discharge point; width_ft
 		# Qup: the upstream seasonal 7Q10 critical Q ft3/s; streamQcfs
 		# Qeff: the seasonal average effluent discharge or annual (with note) if not seasonal in ft3/s; effluentQcfs
 	distPlumePercChronic=(((plume_widthChronic_ft/width_ft*(streamQcfs+effluentQcfs))-effluentQcfs)/streamQcfs)*100 # in percentage
-	if xDownChronic_ft < 0, then chronicPlumePercent=0
-	else
-		if distPlumePercChronic > 0, then chronicPlumePercent=distPlumePercChronic
-		else chronicPlumePercent=0
-		
+	if(xDownChronic_ft < 0){chronicPlumePercent=0}
+	else{
+		if(distPlumePercChronic > 0){chronicPlumePercent=distPlumePercChronic}
+		else{chronicPlumePercent=0}
+	}
 	distPlumePercAcute=(((plume_widthAcute_ft/width_ft*(streamQcfs+effluentQcfs))-effluentQcfs)/streamQcfs)*100 # in percentage
-	if tDownAcute_s < 0, then acutePlumePercent=0
-	else
-		if distPlumePercAcute > 0, then acutePlumePercent=distPlumePercAcute
-		else acutePlumePercent=0	
-
+	if(tDownAcute_s < 0){acutePlumePercent=0}
+	else{
+		if(distPlumePercAcute > 0){acutePlumePercent=distPlumePercAcute}
+		else{acutePlumePercent=0}
+	}
+	
 	# chronic flow limit
 	chronicQLimit=comb_q*chronicPlumePercent
 			     
 	# acute flow limit
-	if distPlumePercAcute < 0.5, then comb_q * distPlumePercAcute
-	else chronicQLimit * 0.5
+	if(distPlumePercAcute < 0.5){acuteQLimit = comb_q * distPlumePercAcute}
+	else{acuteQLimit = chronicQLimit * 0.5}
 	
 	
 	# Gather, return, & print results
-	
+	#chronicQLimit
+	#acuteQLimit
+	#
+	#chronicPlumePercent
+	#acutePlumePercent
+	#
+	#plume_widthChronic_ft
+	#plume_widthAcute_ft
+	#
+	#mixLength
+	#longDispersionCoeff
+	#latDispersionCoeff
+	#froudeNum
+	#shearVel
+	#dist_15min
+	#velocity_ftsec
+	#mannings_n
+	#hyd_rad_ft
+	#area_ft2
 	
 	
 }
