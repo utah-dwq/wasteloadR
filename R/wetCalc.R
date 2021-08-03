d7b67af847fb6e95aa10a3a0150c025c6db1bbe3
Jacob Vanderlaan <jvander@utah.gov>
2021-08-03 12:21:18 -0600

    Update wetCalc.R

--- R/wetCalc.R
+++ R/wetCalc.R
@@ -24,28 +24,55 @@ wetCalc = function(critQ_cfs, effluentQ_cfs, width_ft, plume_width_15min_ft, plu
 	## plume_width_2500ft_ft: Plume width at 2500 ft: determined in mixing zone function (mixZone() output)
 	
 	# Process
-	## calculate acute dilution Q: (Plume width at 15 min or 50%)/W*Qcritical
-	## calculate chronic dilution Q: (Plume width at 2500 ft)/W*Qcritical
+	## calculate acute dilution Q: (Plume width at 15 min or 50%)/W*Qcritical #JV: Is W here river width?
+	acute_dilution_Q=plume_width_15min_ft/width_ft*critQ_cfs
+	
+	## calculate chronic dilution Q: (Plume width at 2500 ft)/W*Qcritical #JV: Is W here river width?
+	chronic_dilution_Q=plume_width_2500ft_ft/width_ft*critQ_cfs	
+	
 	## determine if totally mixed or incompletely mixed
-	#	No - Qeff (cfs) * 2 <= Qup
-	#		ratio of mix to design: (Qcritical/2 + Qeff)/Qeff
-	#			if > 3.33 calc, else end of pipe (EOP)
-	#		acute % effluent: 
-	#			if ratio > 3.33 AND not totally mixed, then EOP
-	#			else Qeff/(0.3* (Qeff + Qcrit/2))
-	#		chronic % effluent: 
-	#			Qeff/(Qeff + Qcrit)
-	#	Yes - Qeff (cfs) * 2 > Qup
+	fully_mixed=ifelse(effluentQ_cfs * 2 <= critQ_cfs, TRUE, FALSE) # Generating separate fully_mixed object so we can print/return it for user review, then used in if() statement below
+	
+	
+	#	Yes - Qeff (cfs) * 2 > Qup # JV to CS: Double my check fully_mixed ifelse above. I interpreted Qeff (cfs) as effluentQ_cfs & Qup as critQ_cfs
 	#		ratio of mix to design: (Qeff + [acute dilution Q])/Qeff
 	#		acute % effluent: 
 	#			if ratio > 3.33 Qeff/(0.3 * [acute dilution Q])
 	#			else EOP
 	#		chronic % effluent: 
 	#			Qeff/(Qeff + [chronic dilution Q])
+	#	No - Qeff (cfs) * 2 <= Qup  
+	#		ratio of mix to design: (Qcritical/2 + Qeff)/Qeff
+	#			if > 3.33 calc, else end of pipe (EOP)
+	#		acute % effluent: 
+	#			if ratio > 3.33 AND not totally mixed, then EOP #JV note to CS: seems to conflict w/ above 'if > 3.33 calc'
+	#			else Qeff/(0.3* (Qeff + Qcrit/2))
+	#		chronic % effluent: 
+	#			Qeff/(Qeff + Qcrit)
 
+	if(fully_mixed){ #Fully mixed
+		mix_design_ratio=(effluentQ_cfs + acute_dilution_Q)/effluentQ_cfs
+		if(mix_design_ratio > 3.33){
+			acute_pct_effluent=effluentQ_cfs/(0.3 * acute_dilution_Q) #If ratio >3.33 & fully_mixed==TRUE
+		}else{
+			acute_pct_effluent="End of pipe" #If ratio <=3.33 & fully_mixed
+		}
+		chronic_pct_effluent=effluentQ_cfs/(effluentQ_cfs + chronic_dilution_Q) #fully_mixed==TRUE
+	}else{ #not fully mixed
+		mix_design_ratio=(critQ_cfs/2 + effluentQ_cfs) / effluentQ_cfs
+		if(mix_design_ratio > 3.33){
+			acute_pct_effluent=effluentQ_cfs/(0.3 * (effluentQ_cfs + critQ_cfs/2)) #If ratio >3.33 & fully_mixed==FALSE
+		}else{
+			acute_pct_effluent="End of pipe" #If ratio <=3.33 & fully_mixed==FALSE
+		}	
+	}
 	
-	
-	
+	# Compile results to list, print, and return
+	result=list(acute_dilution_Q=acute_dilution_Q, chronic_dilution_Q=chronic_dilution_Q, 
+				fully_mixed=fully_mixed, mix_design_ratio=mix_design_ratio, 
+				acute_pct_effluent=acute_pct_effluent, chronic_pct_effluent=chronic_pct_effluent)
+	print(result)
+	return(result)
 }
 
 
