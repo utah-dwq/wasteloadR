69da975ac96c97315c37f102d4a282bf6d4ab94f
Jake Vander Laan <39382528+jakevl@users.noreply.github.com>
2021-08-09 14:21:18 -0600

    Update heatLoad.R

--- R/heatLoad.R
+++ R/heatLoad.R
@@ -4,6 +4,7 @@
  
 #' @param q
 #' @param t
+#' @param alu
 #
 
 #' @return TBD, table, list, etc
@@ -11,14 +12,40 @@
 #' @examples
 
 #' @export
-heatLoad = function(q, t){
-	#calculate heat load (MBTU/d) for receiving and effluent (Q*T*9.7)
+heatLoad = function(q, t, use){
+	# Definitions:
+	## t ??? Receiving water temperature C? Looks like we might also need effluent t.
+	## q - combined effluent + recieving discharge? cfs do we need both recieving water and effluent Qs here again?
+	## alu - aquatic life use. One of 3A, 3B, or 3C
+	
+	#calculate heat load (MBTU/d) for receiving and effluent (Q*T*9.7) - JV what is T here?
+	
+	
+	## Define criteria by use
 	#Use AL Standards (T [3A-20C, others 27C] and deltaT [3A-2C, others 4C])
+	if(!alu %in% c("3A", "3B", "3C")){stop("Error. alu must be one of '3A', '3B', or '3C')}
+	if(alu == "3A"){
+		temp_criterion=20
+		delta_temp=2
+	}else{
+		temp_criterion=27
+		delta_temp=4
+	}
+		
+
 	#calculate combined T load ((Qr*Tr)+(Qd*Td))/(Qr+Qd)
+	combined_t_load=((Qr*Tr)+(Qd*Td))/(Qr+Qd) 
+
 	#calculate T increase (Tcombined - Treceiving)
+	t_increase= combined_t_load - t_receiving
+
 	#check if T+deltaT exceeds standards and note
+	t_increase_exceed=ifelse(t_increase>delta_temp, TRUE, FALSE)
+
 	#calculate seasonal effluent temperature limit summary
 	#	use ((Qu*Tu)+(Qe*Te)) = (Qd*Td) rearranged to Te = ((Qd*Td)-(Qu*Tu))/(Qe)
+	temp_effluent_limit=((Qd*Td)-(Qu*Tu))/(Qe)
+	
 }
 
 
