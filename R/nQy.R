56b5bca3d51833623c3761b6bf2d5c3854024a11
Jacob Vanderlaan <jvander@utah.gov>
2022-07-12 14:11:49 -0600

    Update nQy.R

--- R/nQy.R
+++ R/nQy.R
@@ -5,12 +5,10 @@
 #' @param y Numeric. Recurrence interval in years
 #' @param date_col Column name containing date values. Must be in standard "YYYY-MM-DD" date format.
 #' @param q_col Column name containing discharge values. Any appropriate discharge units are OK. The result will be in the same units as the input.
-#' @param min_obs Minimum number of daily observations to allow an n-day average to be calculated. Default 1. Passed to roll::roll_mean for n-day average calculations. See ?roll::roll_mean for more information regarding usage.
-#' @param out_type One of nQy for low flow statistic output or min_ann_q to see the minimum annual values of n-day average discharges.
 #' @return Either a numeric value of nQy or a dataframe of minimum annual n-day average discharge values.
 #' @import dplyr
 #' @importFrom lubridate year
-#' @importFrom roll roll_mean
+#' @importFrom zoo::rollmean
 #' @examples
 #' # Basic usage
 #' ## Get data
@@ -22,37 +20,32 @@
 #' JR_1700S_7Q10=nQy(data=gauge_data, n=7, y=10, date_col="Date", q_col="discharge_cfs")
 #' JR_1700S_7Q10
 #' 
-#' ## Get min annual flows instead of nQy result
-#' JR_1700S_min_ann_Q=nQy(data=gauge_data, n=7, y=10, date_col="Date", q_col="discharge_cfs", out_type="min_ann_q")
-#' JR_1700S_min_ann_Q
-#' 
 #' ## Other nQy types
-#' JR1700S_30Q2=nQy(data=gauge_data, n=30, y=5, date_col="Date", q_col="discharge_cfs")
-#' JR1700S_30Q2
+#' JR1700S_1Q10=nQy(data=gauge_data, n=1, y=10, date_col="Date", q_col="discharge_cfs")
+#' JR1700S_1Q10
 #' 
-#' # Using plyr::ddply to perform advanced analyses (multiple sites, seasonal, etc.)
-#' 
-#' ## Multiple sites
-#' gauge_data=dataRetrieval::readNWISdv(siteNumbers=c("10171000","10168000","10170500"), startDate="2000-01-01", parameterCd="00060") %>% dplyr::rename(discharge_cfs=X_00060_00003, disch_code=X_00060_00003_cd)# Parameter code 00060 = discharge in cfs. see also ?readNWISuv() to read high frequency values
-#' multi_site_7Q10=plyr::ddply(gauge_data, .variables="site_no", .fun="nQy") # Just default arguments to nQy here which generates 7Q10
-#' multi_site_7Q10
-#' 
-#' ## Split by season
-#' gauge_data=dataRetrieval::readNWISdv(siteNumbers=c("10171000","10168000","10170500"), startDate="2000-01-01", parameterCd="00060") %>% dplyr::rename(discharge_cfs=X_00060_00003, disch_code=X_00060_00003_cd)# Parameter code 00060 = discharge in cfs. see also ?readNWISuv() to read high frequency values
-#' gauge_data$month=lubridate::month(gauge_data$Date)
-#' seasons=data.frame(c("Summer","Summer","Summer", "Fall", "Fall", "Fall", "Winter", "Winter", "Winter", "Spring", "Spring", "Spring"), c(7,8,9,10,11,12,1,2,3,4,5,6))
-#' names(seasons)=c("season", "month")
-#' gauge_data=merge(gauge_data, seasons, all.x=T)
-#' 
-#' multi_site_seasonal_1Q10=plyr::ddply(gauge_data, .variables=c("site_no","season"), .fun="nQy", n=1, y=10) # Note additional arguments to ddply will be passed to the nQy function
-#' multi_site_seasonal_1Q10
-
 
 #' @export
-nQy = function(data, n=7, y=10, date_col="Date", q_col="discharge_cfs", min_obs=1, out_type="nQy"){
+nQy = function(data, n=7, y=10, date_col="Date", q_col="discharge_cfs", plot_fit=TRUE){
 
-	# Generate quantile from y
-	qtile=1/y
+	# Function outline:
+	## Data checks - multiple values on 1 day, implicit NAs
+	## Calculate n-day rolling means
+	## Calculate min annual n-day rolling mean
+	## Rank min annual means and calculate return intervals and exceedance probs
+	## Fit Pearson III distribution (or get percentile or some other approach)
+	## Plot fitted values
+	## Calculate nQy
+	## Export results
+
+	# Development set up
+	#gauge_data=dataRetrieval::readNWISdv(siteNumbers=c("10141000"), startDate="1970-01-01", parameterCd="00060") %>% dplyr::rename(discharge_cfs=X_00060_00003, disch_code=X_00060_00003_cd)
+	#data=gauge_data
+	#date_col="Date"
+	#q_col="discharge_cfs"
+	#n=7
+	#y=10
+	#plot_fit=TRUE
 	
 	# Reduce columns
 	data=data[,c(date_col, q_col)]
@@ -69,31 +62,74 @@ nQy = function(data, n=7, y=10, date_col="Date", q_col="discharge_cfs", min_obs=
 	names(all_dates)="date"
 	data=merge(data, all_dates, all.x=T)
 	if(any(is.na(data$q))){
-		warning("NA discharge values detected. Rolling means will calculated across NAs for periods meeting min_obs requirement. See ?roll::roll_mean for more information regarding min_obs argument.")
+		warning("NA discharge values detected. Rolling means will calculated across NAs.")
 	}
-		
+	
 	# Calculate rolling means
-	rolled_mean=data.frame(roll::roll_mean(data$date, width=n), roll::roll_mean(data$q, width=n, min_obs=min_obs))
-	names(rolled_mean)=c("date","q")
-	rolled_mean=subset(rolled_mean, !is.na(rolled_mean$date))	
-	
-	# Assign year
-	rolled_mean$year=lubridate::year(rolled_mean$date)
-	
-	# Calculate minimum annual flows
-	
-	min_ann_q = rolled_mean %>% dplyr::group_by(year) %>% dplyr::summarize(min_q=min(q), .groups="drop")
-	
-	
-	# Get nQy result
-	
-	nQy=quantile(min_ann_q$min_q, qtile)
+	rolled_means = data %>% mutate(ndaymean = zoo::rollmean(q, n, fill = NA, na.rm = F, align = "right")) 
 
+	# Calculate min annual Q values
+	q_ann_mins <- rolled_means %>% 
+						mutate(year = lubridate::year(date)) %>%
+                        group_by(year) %>%
+                        summarize(minQ = min(ndaymean, na.rm = T), 
+                                  lenDat = length(q),
+                                  lenNAs = sum(is.na(ndaymean))) %>%
+                        filter(lenDat > 328 & lenNAs / lenDat < 0.1) # Only include years missing less than 10% of each year and 10% or fewer NAs
+	
+	# Check for removed years
+	all_years=unique(lubridate::year(data$date))
+	included_years=unique(q_ann_mins$year)
+	removed_years=all_years[!all_years %in% included_years]
+	
+	# Fill zeros in q_ann_mins (need to test w/ Weber River site)
+	q_ann_mins$minQ_zerofilled=q_ann_mins$minQ
+	q_ann_mins$minQ_zerofilled[q_ann_mins$minQ_zerofilled==0]=min(q_ann_mins$minQ[q_ann_mins$minQ!=0])/2 # Zeros filled w/ half of non-zero mins
+	
+	# add rank column and return interval column
+	q_ann_mins <- q_ann_mins %>% 
+                mutate(rank = rank(minQ_zerofilled, ties.method = "first")) %>%
+                mutate(ReturnInterval = (length(rank) + 1)/rank) %>%
+                mutate(ExceedProb = 1 / ReturnInterval)
+
+	# Fit probability distribution
+	## Measures of the distribution
+	Xbar <- mean(log10(q_ann_mins$minQ_zerofilled))
+	S    <- sd(log10(q_ann_mins$minQ_zerofilled))
+	g    <- skewness(log10(q_ann_mins$minQ_zerofilled))
+	
+	## Calculate z, K, to plot the fitted Pearson Type III
+	q_ann_mins <- q_ann_mins %>% 
+	mutate(z = 4.91 * ((1 / ReturnInterval) ^ 0.14 - (1 - 1 / ReturnInterval) ^ 0.14)) %>%
+	mutate(K = (2 / g) * (((1 + (g * z) / 6 - (g ^ 2) / 36) ^ 3) - 1) ) %>%
+	mutate(Qfit = 10^(Xbar + (K * S)))
+	
+	## Plot fitted distribution
+	fit_plot=q_ann_mins %>% 
+		ggplot(aes(x = ReturnInterval, y = minQ_zerofilled, color = "Estimated"))+
+		geom_point()+
+		geom_line(aes(x = ReturnInterval, y = Qfit, color = "Fitted"))+
+		theme_classic()+
+		scale_x_log10()+
+		ylab("n day yearly minimum")+
+		xlab("Return interval")
+	
+	if(plot_fit){fit_plot}
+	
+	# Calculate nQy stat
+	z    <- 4.91 * ((1 / y) ^ 0.14 - (1 - 1 / y) ^ 0.14)
+	K    <- (2 / g) * (((1 + (g * z) / 6 - (g ^ 2) / 36) ^ 3) - 1) 
+	nQy <- 10^(Xbar + K * S)
+	
+	# Print results
+	print(paste0("Years excluded from analysis due insufficient data: "))
+	print(removed_years)
+	print(paste0("Number of zero minimum annual flow values: ", dim(subset(q_ann_mins, minQ==0))[1]))
+	print(paste0("Low flow statistic ", n, "Q", y, ": ", round(nQy,2)))
+	
 	# Return results
-	if(out_type=="min_ann_q"){
-		return(list("nQy"=nQy[[1]], "min_ann_q"=as.data.frame(min_ann_q)))
-	}else{
-		return(nQy[[1]])
-	}
+	results=list("nQy"=nQy, "fit_plot"=fit_plot, "annual_mins_fit"=q_ann_mins[order(q_ann_mins$rank),])
+	return(results)
+	
 }
 
