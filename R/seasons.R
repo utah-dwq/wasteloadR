bab958e1c8449cbd9cbb4ffff3c88c34530dd862
Jacob Vanderlaan <jvander@utah.gov>
2022-07-15 11:56:02 -0600

    Update seasons.R

--- R/seasons.R
+++ R/seasons.R
@@ -17,11 +17,14 @@ seasons=function(dates, seasons=list("winter"=c(1,2,3), "spring"=c(4,5,6), "summ
 
 	seasons_df=stack(seasons)
 	names(seasons_df)=c("month","season")
-	month=data.frame(lubridate::month(dates))
-	names(month)="month"
-	month_seasons=merge(month, seasons_df)
-	
-	season=month_seasons$season
-	return(season)
+	date_month=data.frame(dates, lubridate::month(dates))
+	names(date_month)=c("date","month")
+
+	month_seasons=dplyr::left_join(date_month, seasons_df)
+
+	return(month_seasons$season)
 }
+
+
+
 
